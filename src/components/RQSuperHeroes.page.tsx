53f407738ec029285d9e773af871437e63dc3b31
Zeus <zeus@Zeusui-MacBookAir.local>
2022-04-17 19:37:28 +0900

    react-query data fetching 확인

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,7 +1,14 @@
+import { useQuery } from "react-query"
+import axios from 'axios';
+
 export default function RQSuperHeroesPage(){
+    const results =  useQuery('super-heroes', () => {
+        return axios.get('http://localhost:4000/superheroes');
+    });
+
     return (
         <div>
-            RQSuperHeroes
+            {JSON.stringify(results)}
         </div>
     )
 }
